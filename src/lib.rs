3cf6127dbc19b455db32dc6fbe7c527cc0db998e
softprops <d.tangren@gmail.com>
2019-12-16 01:06:59 -0500

    cleanup imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,6 @@
 mod log;
 pub use log::{metric_scope, MetricLogger, Unit};
-mod serialize;
-//pub use serialize::*;
-mod env;
-//pub use env::*;
 mod config;
-//pub use config::*;
-// re-export for easy of contructing btreemaps
+mod env;
+mod serialize;
 pub use maplit::btreemap as dimensions;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
