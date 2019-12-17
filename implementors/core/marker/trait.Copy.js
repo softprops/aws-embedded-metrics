d8e1f4dbe5981713d9cc6b3008d5e00b8b2c26db
softprops <softprops@users.noreply.github.com>
2019-12-17 01:38:18 +0000

    Automated deployment: Tue Dec 17 01:38:18 UTC 2019 02ba7d84bd7fd4bad96da1d6359e121372e73a3b

---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["aws_embedded_metrics"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"aws_embedded_metrics/enum.Unit.html\" title=\"enum aws_embedded_metrics::Unit\">Unit</a>",synthetic:false,types:["aws_embedded_metrics::log::Unit"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
