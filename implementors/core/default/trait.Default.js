5735c394d0f1ace133773ffac1c81f5560793d03
softprops <softprops@users.noreply.github.com>
2019-12-18 01:33:29 +0000

    Automated deployment: Wed Dec 18 01:33:29 UTC 2019 c27a6f9db64325600f87e608a284f12a9c062234

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["aws_embedded_metrics"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"enum\" href=\"aws_embedded_metrics/enum.Unit.html\" title=\"enum aws_embedded_metrics::Unit\">Unit</a>",synthetic:false,types:["aws_embedded_metrics::log::Unit"]},];
+implementors["aws_embedded_metrics"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"enum\" href=\"aws_embedded_metrics/enum.Unit.html\" title=\"enum aws_embedded_metrics::Unit\">Unit</a>",synthetic:false,types:["aws_embedded_metrics::log::Unit"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"aws_embedded_metrics/struct.MetricLogger.html\" title=\"struct aws_embedded_metrics::MetricLogger\">MetricLogger</a>",synthetic:false,types:["aws_embedded_metrics::log::MetricLogger"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
